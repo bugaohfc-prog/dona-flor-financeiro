b2c5c4dfd91c962840f83ff323e11f7bcccf8ec1
bugaohfc-prog <bugaohfc@gmail.com>
2026-05-02 00:11:21 -0300

    Add files via upload

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,13 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-export function hasSupabaseKey() {
-  return Boolean(
-    import.meta.env.VITE_SUPABASE_URL &&
-    import.meta.env.VITE_SUPABASE_ANON_KEY
-  );
-}
-
-export const supabase = createClient(
+const supabase = createClient(
   import.meta.env.VITE_SUPABASE_URL,
   import.meta.env.VITE_SUPABASE_ANON_KEY
 );
